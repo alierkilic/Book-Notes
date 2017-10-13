41905d770a409532ff721cff5a777e5692b9cd61
Ali Erkilic <erkilic.alican@gmail.com>
2017-10-13 16:50:06 +0300

    Initial Commit

---
+++ Book Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Book Notes
+//
+//  Created by Ali Can Erkilic on 10/13/17.
+//  Copyright © 2017 Ali Can Erkilic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
